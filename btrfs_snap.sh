ce29f11a87c4798cfb902a34c1b2e1cf9f6fb292
AlexNPavel <alexpavel123@gmail.com>
2016-07-14 22:56:34 -0400

    Use correct snapshot location

--- btrfs_snap.sh
+++ btrfs_snap.sh
@@ -15,10 +15,10 @@ logfile=/snapshots.log
 exec > $logfile 2>&1
 day=$(perl -le 'my $wday = (localtime)[6]; print $wday')
 
-btrfs_subs=(/ /boot /home)
+btrfs_subs=(/ /boot/ /home/)
 
 for dir in  ${btrfs_subs[@]}; do
-  cd /.snapshot
+  cd $dir.snapshot
   if [[ -f nightly.6 ]]; then
     if [[ day -eq 0 ]]; then
       shift_weeklies
